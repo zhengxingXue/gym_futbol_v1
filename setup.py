a4d1fef0b15e7095b5d3926ada211da0f83d2f1a
zhengxingXue <zx252cornell@gmail.com>
2020-06-09 23:55:02 -0400

    add dependency

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_futbol',
-      version='1.0.1',
-      install_requires=['gym', 'pymunk']  # And any other dependencies foo needs
+      version='1.0.0',
+      install_requires=['gym', 'pymunk', 'numpy', 'matplotlib', 'opencv-python']  # And any other dependencies foo needs
 )
