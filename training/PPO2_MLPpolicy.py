3f73e2a0e4d4c3678f0290a2e4ae66207ef12fe1
zhengxingXue <zx252cornell@gmail.com>
2020-06-13 12:29:58 -0400

    minor changes

--- training/PPO2_MLPpolicy.py
+++ training/PPO2_MLPpolicy.py
@@ -9,10 +9,13 @@ def main(time_step=10**5):
     log_dir = "./tmp/MlpPolicy/{}".format(int(time.time()))
     os.makedirs(log_dir, exist_ok=True)
     env = create_n_env('futbol-v1', log_dir, 8)
-    eval_callback = create_eval_callback('futbol-v1', save_dir='./logs/MlpPolicy')
+
+    save_dir = './logs/MlpPolicy'
+    eval_callback = create_eval_callback('futbol-v1', save_dir=save_dir)
 
     model = PPO2(MlpPolicy, env, verbose=1)
     model.learn(total_timesteps=time_step, callback=eval_callback)
+    model.save(save_dir + '/' + "ppo2_mlp")
 
 
 if __name__ == "__main__":
