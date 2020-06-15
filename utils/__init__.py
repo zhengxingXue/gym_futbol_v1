72460d34b3aa3136ad0cd04ca1b9dfccef647ac6
zhengxingXue <zx252cornell@gmail.com>
2020-06-15 11:35:49 -0400

    minor changes

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from utils.video_utils import notebook_render_simple, notebook_render_mlp, notebook_render_lstm
-from utils.train_utils import create_eval_callback, create_n_env, create_custom_policy, ppo2_train
+from utils.train_utils import create_eval_callback, create_n_env, create_custom_policy, ppo2_train, \
+    NormalizeObservationWrapper
 from utils.multiagent_utils import MultiAgentWrapper, create_multi_agent_env, ppo2_multi_agent_train, MultiAgentTrain
