306f4720678574092ed24f80b1f5eadb4efa5c38
zhengxingXue <zx252cornell@gmail.com>
2020-06-14 23:55:11 -0400

    add multi agent class for easier training

--- utils/multiagent_utils.py
+++ utils/multiagent_utils.py
@@ -3,8 +3,10 @@ import time
 from stable_baselines import PPO2
 from stable_baselines.bench import Monitor
 from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.common.policies import MlpPolicy
 import gym
 from gym_futbol_v1.envs import Side
+from utils import notebook_render_mlp, notebook_render_lstm
 
 
 class MultiAgentWrapper(gym.Wrapper):
@@ -106,7 +108,6 @@ def ppo2_multi_agent_train(policy, policy_name, env_id='futbol-v1', env_n=8, tim
                                           model_side=Side.right)  # add right model to env
         model_left.set_env(env_left)  # set the new env to model
         model_left.learn(total_timesteps=time_step, reset_num_timesteps=False)
-
         model_left_path = model_path + '-left'
         model_left.save(model_left_path)
         print("left__model saved to " + model_left_path + '.zip')
@@ -118,7 +119,143 @@ def ppo2_multi_agent_train(policy, policy_name, env_id='futbol-v1', env_n=8, tim
         model_right.set_env(env_right)  # set the new env to model
         model_right.learn(total_timesteps=time_step, reset_num_timesteps=False)
         model_right_path = model_path + '-right'
-        model_left.save(model_right_path)
+        model_right.save(model_right_path)
         print("right_model saved to " + model_right_path + '.zip')
 
     return model_left, model_right, save_dir
+
+
+class MultiAgentTrain:
+
+    def __init__(self, env_id='futbol-v1', env_num=8, rl_agent=PPO2, policy=MlpPolicy, policy_name='ppo2-mlp'):
+        self.env_id = env_id
+        self.env_num = env_num
+        self.rl_agent = rl_agent
+        self.policy = policy
+        self.policy_name = policy_name
+        self.verbose = 0
+        self.env_left = gym.make(env_id)
+        self.env_right = gym.make(env_id)
+        self.log_dir = './'
+        self.save_dir = './'
+        self.set_up_dir()
+        self._set_up_envs()
+        self._set_up_models()
+        self.turn = 0
+        self.total_time_step = 0
+        self.left_time_step = 0
+        self.right_time_step = 0
+
+    def get_info(self):
+        print('Training Turn : {}'.format(self.turn))
+        print('Total Time Step : {}'.format(self.total_time_step))
+        print('Model Left Time Step : {}'.format(self.left_time_step))
+        print('Model Right Time Step : {}'.format(self.right_time_step))
+        print('Model Save Directory : {}'.format(self.save_dir))
+        print('Model Log Directory : {}'.format(self.log_dir))
+        print('Verbose : ' + str(self.verbose))
+
+    def set_up_dir(self, save_dir_prefix='./training/logs', log_dir_prefix='./training/tmp'):
+        time_str = "{}".format(int(time.time()))
+        self.log_dir = log_dir_prefix + "/" + self.policy_name + '-multi-agent-' + time_str
+        os.makedirs(self.log_dir, exist_ok=True)
+        self.save_dir = save_dir_prefix + '/' + self.policy_name + '-multi-agent-' + time_str
+        os.makedirs(self.save_dir, exist_ok=True)
+
+    def _set_up_envs(self):
+        self.set_up_env(Side.left, random_op=True, set_model=False)
+        self.set_up_env(Side.right, random_op=True, set_model=False)
+
+    def _set_up_models(self):
+        self.model_left = self.rl_agent(self.policy, self.env_left, verbose=self.verbose)
+        self.model_right = self.rl_agent(self.policy, self.env_right, verbose=self.verbose)
+
+    def set_up_env(self, side, random_op=False, set_model=True):
+        """
+        :param side: which side of env to set up
+        :param random_op: whether the env's opponents' actions are random.
+        """
+        if side == Side.left:
+            if random_op:
+                self.env_left = create_multi_agent_env(self.env_id, Side.left, log_dir=self.log_dir,
+                                                       num_envs=self.env_num,
+                                                       set_model=False, model=None, model_side=None)
+            else:
+                self.env_left = create_multi_agent_env(self.env_id, Side.left, log_dir=self.log_dir,
+                                                       num_envs=self.env_num,
+                                                       set_model=True, model=self.model_right,
+                                                       model_side=Side.right)  # add right model to env
+            if set_model:
+                self.model_left.set_env(self.env_left)
+        elif side == Side.right:
+            if random_op:
+                self.env_right = create_multi_agent_env(self.env_id, Side.right, log_dir=self.log_dir,
+                                                        num_envs=self.env_num,
+                                                        set_model=False, model=None, model_side=None)
+            else:
+                self.env_right = create_multi_agent_env(self.env_id, Side.right, log_dir=self.log_dir,
+                                                        num_envs=self.env_num,
+                                                        set_model=True, model=self.model_left,
+                                                        model_side=Side.left)  # add left model to env
+            if set_model:
+                self.model_right.set_env(self.env_right)
+
+    def train(self, num_turn=10, time_step=10 ** 4, save=True, verbose=0):
+        self.change_verbose(verbose)
+        current_turn = self.turn
+        for i in range(num_turn):
+            model_path = self.save_dir + '/' + self.policy_name + '-' + str(i+current_turn)
+
+            model_left_path = model_path + '-left'
+            self.train_left(time_step=time_step, save=save, save_path=model_left_path, random_right=False)
+
+            model_right_path = model_path + '-right'
+            self.train_right(time_step=time_step, save=save, save_path=model_right_path, random_left=False)
+
+            self.turn += 1
+
+    def train_left(self, time_step=10 ** 4, save=False, save_path='./', random_right=False):
+        self.set_up_env(Side.left, random_op=random_right)
+        self.model_left.learn(total_timesteps=time_step, reset_num_timesteps=False)
+        self.left_time_step += time_step
+        self.total_time_step += time_step
+        if save:
+            self.model_left.save(save_path)
+            print("left__model saved to " + save_path + '.zip')
+
+    def train_right(self, time_step=10 ** 4, save=False, save_path='./', random_left=False):
+        self.set_up_env(Side.right, random_op=random_left)
+        self.model_right.learn(total_timesteps=time_step, reset_num_timesteps=False)
+        self.right_time_step += time_step
+        self.total_time_step += time_step
+        if save:
+            self.model_right.save(save_path)
+            print("right_model saved to " + save_path + '.zip')
+
+    def change_verbose(self, verbose):
+        """
+        :param verbose: 0 for no train info, 1 for train info
+        """
+        self.verbose = verbose
+        self.model_left.verbose = verbose
+        self.model_right.verbose = verbose
+
+    def notebook_render_left(self, random_right=False):
+        if random_right:
+            notebook_render_mlp(self.env_id, self.model_left, side=Side.left)
+        else:
+            self.env_left = MultiAgentWrapper(gym.make(self.env_id), Side.left)
+            self.env_left.set_agent(self.model_right, Side.right)
+            _ = notebook_render_mlp(self.env_left, self.model_left, length=300, side=Side.left)
+
+    def notebook_render_right(self, random_left=False):
+        if random_left:
+            notebook_render_mlp(self.env_id, self.model_right, side=Side.right)
+        else:
+            self.env_right = MultiAgentWrapper(gym.make(self.env_id), Side.right)
+            self.env_right.set_agent(self.model_left, Side.left)
+            _ = notebook_render_mlp(self.env_right, self.model_right, length=300, side=Side.right)
+
+    def save_models(self):
+        self.model_left.save(self.save_dir + '/' + self.policy_name + '-left')
+        self.model_right.save(self.save_dir + '/' + self.policy_name + '-right')
